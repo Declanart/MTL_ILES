42a660f1f2f1e5cf35cacd6645519f82938805e2
Declanart <keven.brunelle.mtl@gmail.com>
2025-09-17 11:05:05 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: change base to "/<repo-name>/" before deploying to GitHub Pages.
 export default defineConfig({
   plugins: [react()],
-  base: '/MTL_ILES/', // ← replace REPO_NAME with your GitHub repo name
+  base: '/kilometres-ensemble/',
 })
